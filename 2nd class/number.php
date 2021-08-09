2498e1e76d09f51a2f18f05b644b7f43b2a6808c
unknown <freelancing.bijoy@gmail.com>
2021-08-09 10:20:45 +0600

    learn number

---
+++ 2nd class/number.php
@@ -0,0 +1,49 @@
+<?php
+
+//number
+
+//declare number 
+
+$number1 = 10;
+$number2 = 20;
+$number3 = 30;
+
+//arethmatic operation
+
+$sum = $number1 * $number2;
+echo $sum;
+
+// assignment wth operator
+
+$number1 += $number2;
+echo "<br>";
+// echo $number1;
+
+$number2 *= $number3;
+echo "<br>";
+// echo $number2;
+
+//increment operator
+
+$a = 5;
+
+
+// echo $a++; //5 post increment
+// echo "<br>";//6
+// echo $a;
+echo "<br>";
+
+$b= 10;
+$c=1.2558;
+
+// echo$b--; //post decrement
+// echo "<br>";
+// echo$b--;
+
+// echo --$b;
+
+//number checking function
+
+echo  is_integer($b);
+echo "<br>";
+echo  is_float($c);
