c0613f50a0030033fc6fb67658a96d01f5f04c19
Iambijoy656 <freelancing.bijoy.com>
2021-08-15 18:08:00 +0600

    switch and loop

---
+++ 4th class/switch.php
@@ -0,0 +1,37 @@
+<?php
+
+///if else if else
+
+// $num=10;
+
+// if ($num>20){
+//     echo "number is less than 20";
+
+// }elseif ($num ===18) {
+//     echo "number is equal 18";
+// }else {
+//     echo "nummber is not equal 20";
+// }
+
+
+
+// switch
+
+$color="purple";
+
+switch ($color) {
+    case 'red':
+        echo "i am red";
+        break;
+    case 'green':
+            echo "i am green";
+        break;
+
+    case 'blue':
+    echo "i am blue";
+      break;
+    
+    default:
+        echo "invalid color";
+        break;
+}
